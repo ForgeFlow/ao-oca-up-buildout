d422a860513eceec563904c7f71ddfec2451a997
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-07-26 19:03:25 +0200

    update user

--- migrate.sh
+++ migrate.sh
@@ -7,11 +7,11 @@ if [ -z "$DATABASE" ]; then
     exit 1
 fi
 
-python3 ./pre-migration.py --db_name=$DATABASE --db_user=odoo --db_password=odoo --db_host localhost --db_port=5432
+python3 ./pre-migration.py --db_name=$DATABASE --db_user=oca --db_password= --db_host localhost --db_port=5432
 
 VERSIONS=$(eval echo bin/start_openupgrade*)
 for version in $VERSIONS; do
-    $version --db_user odoo --db_password=odoo --update=all --stop-after-init --database=$DATABASE || exit 1
+    $version --db_user oca --db_password= --update=all --stop-after-init --database=$DATABASE || exit 1
 done
 
-python3 ./post-migration.py --db_name=$DATABASE --db_user=odoo --db_password=odoo --db_host localhost --db_port=5432
+python3 ./post-migration.py --db_name=$DATABASE --db_user=oca --db_password= --db_host localhost --db_port=5432
